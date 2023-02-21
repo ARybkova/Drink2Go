2d456bad5097be305a2cd83b51a3ebd75e6745e9
Alyona Rybkova <alyona.vorobyova.94@mail.ru>
2023-02-21 16:05:47 +0700

    Добавляет стилизацию промоблока, блока преимуществ, каталога для всех разрешений

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,36 @@
-/* в этот файл добавляет скрипты*/
-let navMain = document.querySelector(".main-navigation");
-let navToggle = document.querySelector(".main-navigation__toggle");
+/* Бургер-меню*/
+const burgerMenu = () => {
+  let navMain = document.querySelector(".main-navigation");
+  let navToggle = document.querySelector(".main-navigation__toggle");
 
-navMain.classList.remove("main-navigation--nojs");
-navMain.classList.add("main-navigation--closed");
+  navMain.classList.remove("main-navigation--nojs");
+  navMain.classList.add("main-navigation--closed");
 
-navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("main-navigation--closed")) {
-    navMain.classList.remove("main-navigation--closed");
-    navMain.classList.add("main-navigation--opened");
-  } else {
-    navMain.classList.add("main-navigation--closed");
-    navMain.classList.remove("main-navigation--opened");
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-navigation--closed")) {
+      navMain.classList.remove("main-navigation--closed");
+      navMain.classList.add("main-navigation--opened");
+    } else {
+      navMain.classList.add("main-navigation--closed");
+      navMain.classList.remove("main-navigation--opened");
+    }
+  });
+}
+
+burgerMenu();
+
+/* Slider */
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   }
 });
